d95b1ceb0fc8c46dbe01b6ec7be5e5ca1398532a
Ahmed Sadek <69456197+A-Sadek@users.noreply.github.com>
2024-11-19 16:01:14 +0100

    Update 03_run_af_monomer.sh
    clean up

--- design_optimisation/03_run_af_monomer.sh
+++ design_optimisation/03_run_af_monomer.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-## This script is to be run on Izar (GPU cluster)
-## This script will select the top 5 MPNN designs based on the global score and submit the results to AF2 for monomer prediction using single sequence mode
+## This script is to be run on GPU cluster with with SLURM managment system
+## This script will select the top 5 unique ProteinMPNN sequences/design based on the global score and submit the results to AF2 for monomer prediction using single sequence mode
+## This script uses ColabFold for running the predictions
 
-python /work/lpdi/users/asadek/scaffold_opt_project/scripts/src/run_af_monomer.py --mpnn_folder {Path to folder containing MPNN run outputs} \
-                                                                                  --num_of_seqs 5 \
-                                                                                  --path_to_conda {Path to source conda.sh, e.g. "/home/asadek/miniconda3/etc/profile.d/conda.sh"} \
-                                                                                  --path_to_env {Path to activate conda environment, e.g. "/home/asadek/miniconda3/envs/ColabFold"} \
+python {PATH_to_src_DIR}/src/run_af_monomer.py --mpnn_folder {Path to folder containing MPNN run outputs, e.g., "./sol_mpnn_fixed_interface/"} \
+                                               --num_of_seqs 5 \
+                                               --path_to_conda {Path to source conda.sh} \
+                                               --path_to_env {Path to activate conda environment} \

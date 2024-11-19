d2c296aa9a1947f6433d9f5681425bd243d61b57
Ahmed Sadek <69456197+A-Sadek@users.noreply.github.com>
2024-11-19 16:06:00 +0100

    Update 04_run_af_monomer_analysis.sh

--- design_optimisation/04_run_af_monomer_analysis.sh
+++ design_optimisation/04_run_af_monomer_analysis.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-## This script is to be run on Jed (CPU cluster)
-## This script will select the top folded designs from  AF2 monomer based on pLDDT>=80 and CA RMSD <= 1.5 from binder
-## Then these designs will be submitted for AF2 multimer predictions
+## This script is to be run on a CPU cluster
+## This script will select the top folded designs from  AF2 monomer predictions based on pLDDT>=80 and CA RMSD to model <= 1.5
 
 python /work/lpdi/users/asadek/scaffold_opt_project/scripts/src/run_af2_monomer_analysis.py --af_pred_folder /work/lpdi/users/asadek/scaffold_opt_project/targets/pdl1/original_designs_af_monomer/af_pred/ \
                                                                                             --rs_models_dir /work/lpdi/users/asadek/scaffold_opt_project/targets/pdl1/5jds_selected_all/ \

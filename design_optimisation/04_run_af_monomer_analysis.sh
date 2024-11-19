10b9b48fe3db156a197ec9a7126dc681365c81a2
Ahmed Sadek <69456197+A-Sadek@users.noreply.github.com>
2024-11-19 16:06:39 +0100

    Update 04_run_af_monomer_analysis.sh

--- design_optimisation/04_run_af_monomer_analysis.sh
+++ design_optimisation/04_run_af_monomer_analysis.sh
@@ -4,6 +4,6 @@
 ## This script will select the top folded designs from  AF2 monomer predictions based on pLDDT>=80 and CA RMSD to model <= 1.5
 
 python /work/lpdi/users/asadek/scaffold_opt_project/scripts/src/run_af2_monomer_analysis.py --af_pred_folder /work/lpdi/users/asadek/scaffold_opt_project/targets/pdl1/original_designs_af_monomer/af_pred/ \
-                                                                                            --rs_models_dir /work/lpdi/users/asadek/scaffold_opt_project/targets/pdl1/5jds_selected_all/ \
+                                                                                            --rs_models_dir {Path to folder containing rosetta models} \
                                                                                             --rs_model_binder_chain 'B' \
                                                                                             --af_model_binder_chain 'A'

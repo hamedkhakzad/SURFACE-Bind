0d3a382e5a7f7abe01adcb984db7f7d877ab1b08
Ahmed Sadek <69456197+A-Sadek@users.noreply.github.com>
2024-11-19 15:47:08 +0100

    Update 02_run_sol_mpnn_da.sh
    clean up

--- design_optimisation/02_run_sol_mpnn_da.sh
+++ design_optimisation/02_run_sol_mpnn_da.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-## This script is to be run on Jed (CPU cluster)
-## This script is to use ProteinMPNN to design a provided design(s)
+## This script is to be run on a CPU cluster with SLURM managment system
+## This script is to use ProteinMPNN to fully design provided design(s)
+## In this example, we are generating 25 sequences for our binder chain "B"
 
-python /work/lpdi/users/asadek/scaffold_opt_project/scripts/src/run_sol_mpnn_opt.py --rs_models_dir {Path to folder containing rosetta models} \
-                                                                                    --pmpnn_repo {Path to ProteinMPNN repo} \
-                                                                                    --path_to_conda {Path to source conda.sh} \
-                                                                                    --path_to_env {Path to activate conda environment} \
-                                                                                    --binder_chain {Binder Chain ID} \
-                                                                                    --seqs_to_generate 25 \
-                                                                                    --optimisation_strategy design_interface
+python {PATH_TO_src_DIR}/src/run_sol_mpnn_opt.py --rs_models_dir {Path to folder containing rosetta models} \
+                                                 --pmpnn_repo {Path to ProteinMPNN repo} \
+                                                 --path_to_conda {Path to source conda.sh} \
+                                                 --path_to_env {Path to activate conda environment} \
+                                                 --binder_chain {Binder Chain ID} \
+                                                 --seqs_to_generate 25 \
+                                                 --optimisation_strategy design_interface
 
 

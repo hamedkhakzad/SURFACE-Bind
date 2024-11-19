baffee71016dd351de287de2d44a0755c83bde85
Ahmed Sadek <69456197+A-Sadek@users.noreply.github.com>
2024-11-19 15:44:20 +0100

    Update 01_run_sol_mpnn_fh.sh
    clean up

--- design_optimisation/01_run_sol_mpnn_fh.sh
+++ design_optimisation/01_run_sol_mpnn_fh.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-## This script is to be run on Jed (CPU cluster)
-## This script is to use ProteinMPNN to design a provided design(s) excpet the interaction interface within the provided cutoff
+## This script is to be run on a CPU cluster with SLURM managment system
+## This script is to use ProteinMPNN to design provided design(s) excpet the interaction interface within the provided cutoff
+## In this example, we are generating 25 sequences for our binder chain "B" while fixing residues 3.5Å away from the target protein
 
 python /work/lpdi/users/asadek/scaffold_opt_project/scripts/src/run_sol_mpnn_opt.py --rs_models_dir {Path to folder containing rosetta models} \
                                                                                     --pmpnn_repo {Path to ProteinMPNN repo} \
                                                                                     --path_to_conda {Path to source conda.sh} \
                                                                                     --path_to_env {Path to activate conda environment} \
                                                                                     --scripts_src_repo {Path to scripts src repo} \
-                                                                                    --binder_chain {Binder Chain ID} \
+                                                                                    --binder_chain "B" \
                                                                                     --interface_threshold 3.5 \
                                                                                     --seqs_to_generate 25 \
                                                                                     --optimisation_strategy fixed_interface

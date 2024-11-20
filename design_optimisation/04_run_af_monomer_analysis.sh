067cf7a045d1474c5b496ea17cf4b79a90a6534a
Ahmed Sadek <69456197+A-Sadek@users.noreply.github.com>
2024-11-20 14:15:14 +0100

    Update 04_run_af_monomer_analysis.sh

--- design_optimisation/04_run_af_monomer_analysis.sh
+++ design_optimisation/04_run_af_monomer_analysis.sh
@@ -3,9 +3,9 @@
 ## This script is to be run on a CPU cluster
 ## This script will select the top folded designs from  AF2 monomer predictions based on pLDDT>=80 and CA RMSD to model <= 1.5
 
-python /work/lpdi/users/asadek/scaffold_opt_project/scripts/src/run_af2_monomer_analysis.py --mpnn_folder {Path to folder containing MPNN run outputs, e.g., "./sol_mpnn_fixed_interface/"} \
-                                                                                            --rs_models_dir {Path to folder containing rosetta models} \
-                                                                                            --rs_model_binder_chain 'B' \
-                                                                                            --af_model_binder_chain 'A' \
-                                                                                            --pLDDT_thresh 80.0 \
-                                                                                            --RMSD_thresh 1.5 
+python {PATH_to_src_DIR}/run_af2_monomer_analysis.py --mpnn_folder {Path to folder containing MPNN run outputs, e.g., "./sol_mpnn_fixed_interface/"} \
+                                                     --rs_models_dir {Path to folder containing rosetta models} \
+                                                     --rs_model_binder_chain 'B' \
+                                                     --af_model_binder_chain 'A' \
+                                                     --pLDDT_thresh 80.0 \
+                                                     --RMSD_thresh 1.5 

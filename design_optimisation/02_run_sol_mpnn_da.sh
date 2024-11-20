9ea93357478b293be63c2a602270878cf288b93d
Ahmed Sadek <69456197+A-Sadek@users.noreply.github.com>
2024-11-20 14:06:55 +0100

    Update 02_run_sol_mpnn_da.sh

--- design_optimisation/02_run_sol_mpnn_da.sh
+++ design_optimisation/02_run_sol_mpnn_da.sh
@@ -4,12 +4,12 @@
 ## This script is to use ProteinMPNN to fully design provided design(s)
 ## In this example, we are generating 25 sequences for our binder chain "B"
 
-python {PATH_TO_src_DIR}/src/run_sol_mpnn_opt.py --rs_models_dir {Path to folder containing rosetta models} \
-                                                 --pmpnn_repo {Path to ProteinMPNN repo} \
-                                                 --path_to_conda {Path to source conda.sh} \
-                                                 --path_to_env {Path to activate conda environment} \
-                                                 --binder_chain {Binder Chain ID} \
-                                                 --seqs_to_generate 25 \
-                                                 --optimisation_strategy design_interface
+python {PATH_TO_src_DIR}/run_sol_mpnn_opt.py --rs_models_dir {Path to folder containing rosetta models} \
+                                             --pmpnn_repo {Path to ProteinMPNN repo} \
+                                             --path_to_conda {Path to source conda.sh} \
+                                             --path_to_env {Path to activate conda environment} \
+                                             --binder_chain "B" \
+                                             --seqs_to_generate 25 \
+                                             --optimisation_strategy design_interface
 
 
